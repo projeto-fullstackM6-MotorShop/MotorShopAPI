f813fb94cc77a786e332acf77aa0eb3218f26d7b
Clayson <claysonmj@gmail.com>
2023-04-27 00:33:33 -0300

    feat::hammer: Ad creation updated to receive images

--- src/services/announcement/createAnnouncement.service.ts
+++ src/services/announcement/createAnnouncement.service.ts
@@ -5,6 +5,7 @@ import {
 import { AppDataSource } from "../../data-source";
 import { Repository } from "typeorm";
 import Announcement from "../../entities/announce.entity";
+import { Image } from "../../entities/image.entity";
 
 const createAnnouncementService = async (
   payload: IAnnouncement,
@@ -13,22 +14,40 @@ const createAnnouncementService = async (
   const announcementRepository: Repository<Announcement> =
     AppDataSource.getRepository(Announcement);
 
-  const { fipe, price } = payload;
+    const imagesRepository = AppDataSource.getRepository(Image)
+    
+    const { fipe, price ,images} = payload;
+    
+    const percentValue = fipe * 0.05;
+    const is_good_price = price <= fipe - percentValue;
 
-  const percentValue = fipe * 0.05;
-  const is_good_price = price <= fipe - percentValue;
+    if (is_good_price) {
+      payload.is_good_price = true;
+    }
 
-  if (is_good_price) {
-    payload.is_good_price = true;
-  }
+    const newAnnouncement = announcementRepository.create({
+      ...payload,
+      user: user,
+    });
 
-  const newAnnouncement = announcementRepository.create({
-    ...payload,
-    user: user,
-  });
-  await announcementRepository.save(newAnnouncement);
+    if(images){
+      const newImages = images.map(image => imagesRepository.create({imageUrl:image}))
+      const imagensSave = await imagesRepository.save(newImages)
+      newAnnouncement.image = imagensSave
+    }
 
-  return newAnnouncement;
+    await announcementRepository.save(newAnnouncement);
+
+    const returnAnnouncement = await announcementRepository.findOne({
+      where: {
+        id: newAnnouncement.id,
+      },
+      relations: {
+        image: true,
+      },
+    });
+
+  return returnAnnouncement!;
 };
 
 export default createAnnouncementService;
