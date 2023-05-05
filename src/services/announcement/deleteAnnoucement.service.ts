0cca37fc94896c00e39a60d117fba4558f9bd66d
Igor <silveira.igor@outlook.com>
2023-05-05 10:19:11 -0300

    fix: fix problem in delete annoucement

--- src/services/announcement/deleteAnnoucement.service.ts
+++ src/services/announcement/deleteAnnoucement.service.ts
@@ -1,20 +1,25 @@
 import { AppDataSource } from "../../data-source";
 import Announcement from "../../entities/announce.entity";
+import Comment from "../../entities/comment.entity";
 import { Image } from "../../entities/image.entity";
 
 const deleteAnnoucementService = async (id: string): Promise<void> => {
   const annoucementRepository = AppDataSource.getRepository(Announcement);
-  const imgRepo = AppDataSource.getRepository(Image)
+  const imageRepository = AppDataSource.getRepository(Image);
+  const commentRepository = AppDataSource.getRepository(Comment);
 
   const findAnnoucement = await annoucementRepository.findOne({
     where: { id: id },
-    relations: { image: true },
+    relations: { image: true, comments: true },
   });
 
-  findAnnoucement?.image.map(img => {
-    imgRepo.delete({id:img.id})
-  })
+  findAnnoucement?.image.forEach((img) => {
+    imageRepository.delete({ id: img.id });
+  });
 
+  findAnnoucement?.comments.forEach((comment) => {
+    commentRepository.delete({ id: comment.id });
+  });
 
   await annoucementRepository.delete({ id: id });
 };
